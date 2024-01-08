98f2ef24c664e63088784fa57de661de5a1c73e5
Ndriçim_Halili <ndriqim_@hotmail.com>
2024-01-08 12:12:07 +0100

    All hyjnesha results

--- Assignment 2 - OpenMP/Assignment 2 - OpenMP.cpp
+++ Assignment 2 - OpenMP/Assignment 2 - OpenMP.cpp
@@ -5,13 +5,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 const double epsilon = 1e-9;
 
 
-const int N = 20000;
+const int N = 46080;
 
 void applySequential(float** matrix) {
 
@@ -25,32 +26,14 @@ void applySequential(float** matrix) {
 			matrix[i][j] = (term1 + term2 + term3) * 100;
 		}
 	}
-	//Print the matrix
-	/*for (auto& i : matrix)
-	{
-		std::cout << "\n";
-		for (const double j : i)
-		{
-			std::cout << j << " ";
-		}
-	}*/
 }
 
 
 void applyParallel(float** matrix, int numberOfThreads) {
-	//    omp_set_num_threads(8);
-	//#pragma omp parallel
 	{
 
 		//Use the formula to fill the matrix
-#pragma omp parallel for num_threads(numberOfThreads)
-/*for (int i = 1; i < N; i++)
-{
-	for (size_t j = 1; j < N; j++)
-	{
-		matrix[i][j] = fabs(sin(matrix[i - 1][j - 1])) + fabs(sin(matrix[i][j - 1])) + fabs(sin(matrix[i - 1][j])) * 100;
-	}
-}*/
+		#pragma omp parallel for num_threads(numberOfThreads)
 
 		for (int i = 1; i < N; i++) {
 			for (int j = 1; j < N; j++) {
@@ -72,9 +55,9 @@ void showResults(float** matrix, int N, int row1, int col1, int row2, int col2, 
 	float u2 = matrix[row2][col2];
 	float u3 = matrix[row3][col3];
 
-	string res1 = std::abs(u1 - v1) < epsilon ? "Ok" : "Not ok";
-	string res2 = std::abs(u2 - v2) < epsilon ? "Ok" : "Not ok";
-	string res3 = std::abs(u3 - v3) < epsilon ? "Ok" : "Not ok";
+	string res1 = fabs(u1 - v1) < epsilon ? "Ok" : "Not ok";
+	string res2 = fabs(u2 - v2) < epsilon ? "Ok" : "Not ok";
+	string res3 = fabs(u3 - v3) < epsilon ? "Ok" : "Not ok";
 
 	cout << "A[" << row1 << "][" << col1 << "]: " << u1 << " - " << res1 << "\n";
 	cout << "A[" << row2 << "][" << col2 << "]: " << u2 << " - " << res2 << "\n";
@@ -205,41 +188,5 @@ int main(int argc, char** argv)
 		number_of_threads *= 2;
 	}
 
-	//   //Start timer
-	//   start = omp_get_wtime();
-	//   //Call the function
-	//   applyParallel(parallelMatrix, x);
-	   ////Stop timer
-	   //end = omp_get_wtime();
-	//   //Print the time
-	//   /*std::cout << "\n\nTime: " << end - start << " seconds\n";*/
-
-	//   cout << "Execution time for parallel computation with " << x << " threads: " << end - start << endl;
-
-	//   showResults(parallelMatrix, N, row1, col1, row2, col2, row3, col3);
-
-
-
-
-
-	   //std::cout << "Hello World!\n";
-
 	return 0;
 }
-
-
-
-
-
-//Create a function
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
